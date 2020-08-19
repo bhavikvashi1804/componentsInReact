3ee9c5331ad642765b8babc93dfebc1a81849a8b
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-19 12:10:39 +0530

    Create more 2 comments by copy and paste the comment div

--- src/index.js
+++ src/index.js
@@ -21,6 +21,38 @@ const App = () => {
           </div>
         </div>
       </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()}/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Raj Patel
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 7:00</span>
+          </div>
+          <div className="text">
+            <span className="date">Great work</span>
+          </div>
+        </div>
+      </div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()}/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Yash Patel
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 4:00</span>
+          </div>
+          <div className="text">
+            <span className="date">Nice man</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
