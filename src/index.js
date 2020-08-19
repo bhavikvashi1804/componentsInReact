c6fa3c9279091aa26c6e505b5d608cd21a0048d7
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-19 12:38:02 +0530

    Task: props pass the image src

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 import CommentDetail from "./components/CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Bhavik Vashi" createdAt="Today at 6:00 PM" comment="Good Work"/>
-      <CommentDetail author="Yash Patel" createdAt="Today at 5:00 AM" comment="Nice Work"/>
-      <CommentDetail author="Raj Patel" createdAt="Yesterday at 6:00 PM" comment="Great man"/>
+      <CommentDetail author="Bhavik Vashi" createdAt="Today at 6:00 PM" comment="Good Work" img={faker.image.avatar()}/>
+      <CommentDetail author="Yash Patel" createdAt="Today at 5:00 AM" comment="Nice Work" img={faker.image.avatar()}/>
+      <CommentDetail author="Raj Patel" createdAt="Yesterday at 6:00 PM" comment="Great man" img={faker.image.avatar()}/>
     </div>
   );
 };
