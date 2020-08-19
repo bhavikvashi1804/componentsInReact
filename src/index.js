dd1cceda91c90cd7eb5f03987554fc5f7da0a559
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-19 12:31:39 +0530

    passing the props from parent component to child component

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from "./components/CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail author="Bhavik Vashi" createdAt="Today at 6:00 PM" comment="Good Work"/>
+      <CommentDetail author="Yash Patel" createdAt="Today at 5:00 AM" comment="Nice Work"/>
+      <CommentDetail author="Raj Patel" createdAt="Yesterday at 6:00 PM" comment="Great man"/>
     </div>
   );
 };
