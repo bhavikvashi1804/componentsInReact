b6b9f20ee8541369a5642a1af1bf520ad5e29426
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-19 11:58:18 +0530

    Comment UI

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const App=()=>{
-    return (<h1>Hello</h1>);
-}
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Bhavik Vashi
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00</span>
+          </div>
+          <div className="text">
+            <span className="date">Great job</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
